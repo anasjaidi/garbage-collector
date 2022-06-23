2c5792216c1af6b1e915ea8c44771f3e51b47d9f
anas jaidi <anas.jaidi@icloud.com>
2022-06-23 16:45:22 +0100

    add new node function for linked list methode

--- collector.c
+++ collector.c
@@ -3,10 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   collector.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anasjaidi <anasjaidi@student.42.fr>        +#+  +:+       +#+        */
+/*   By: ajaidi <ajaidi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 15:50:27 by anasjaidi         #+#    #+#             */
-/*   Updated: 2022/06/23 15:50:32 by anasjaidi        ###   ########.fr       */
+/*   Updated: 2022/06/23 16:44:44 by ajaidi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "collector.h"
+
+
+
+// start linked list methode
+
+t_collector	*new_node(void *adr)
+{
+	t_collector	*node;
+
+	node = malloc(sizeof(t_collector));
+	if (!node)
+		return (null);
+	node->adr = adr;
+	node->next = null;
+	return (node);
+}
+
+// end linked list methode
--- collector.h
+++ collector.h
@@ -1,6 +1,17 @@
 # ifndef COLLECOR_H
 # define COLLECTOR_H
 
+// start including libs
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+# define null NULL
+
+// end including 
+
+
+
 // start structure for linked list methode
 
 typedef struct collector
