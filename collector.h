b8de5b5013e527f7a150f0fb788f43e5b30b2430
anas jaidi <anas.jaidi@icloud.com>
2022-06-23 16:08:53 +0100

    add linked list methode structure

---
+++ collector.h
@@ -0,0 +1,16 @@
+# ifndef COLLECOR_H
+# define COLLECTOR_H
+
+// start structure for linked list methode
+
+typedef struct collector
+{
+	void				*adr;
+	struct collector	*next;
+}	t_collector;
+
+
+// end linked list methode
+
+
+# endif
